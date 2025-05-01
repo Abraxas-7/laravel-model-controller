7bb846817aa535716bf4d472eb76be06a04a0371
Abraxas-7 <arber.beshaj.02@gmail.com>
2025-05-01 23:07:04 +0200

    feat: implement movie display with card component and add FontAwesome icons

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,39 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+@php
+    // $vote non è un numero va convertito
+    $votestring = trim($vote);
+    $votecasted = floatval($votestring);
+
+    // calcolo stelle piene, floor arrotonda per difetto
+    $solidstar = floor($votecasted);
+
+    if ($votecasted - $solidstar >= 0.5) {
+        $halfstar = 1;
+    } else {
+        $halfstar = 0;
+    }
+    $emptystar = 10 - $solidstar - $halfstar;
+@endphp
+
+<div class="card h-100">
+    <div class="card-body">
+        <h4>{{ $title }}</h4>
+        <h5 class="text-muted">{{ $original_title }}</h5>
+        <div class="pb-3 pt-1">{{ $nationality }}</div>
+        <div class="py-2"> {{ $date }}</div>
+    </div>
+    <div class="card-footer bg-primary-subtle">
+        <div>
+            @for ($i = 0; $i < $solidstar; $i++)
+                <i class="fa-solid fa-star text-primary"></i>
+            @endfor
+            @for ($i = 0; $i < $halfstar; $i++)
+                <i class="fa-regular fa-star-half-stroke text-primary"></i>
+            @endfor
+            @for ($i = 0; $i < $emptystar; $i++)
+                <i class="fa-regular fa-star text-primary"></i>
+            @endfor
+
+            <span class="text-primary px-2"> ({{ $vote }}) </span>
+        </div>
+    </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,29 @@
 @section('title', 'Home')
 
 @section('content')
-    <div>
-        contenuto pagina
+    <div class="container py-4 debug">
+        <div class="row row-cols-3 g-3">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <x-card>
+                        <x-slot name="title">
+                            {{ $movie->title }}
+                        </x-slot>
+                        <x-slot name="original_title">
+                            {{ $movie->original_title }}
+                        </x-slot>
+                        <x-slot name="nationality">
+                            {{ $movie->nationality }}
+                        </x-slot>
+                        <x-slot name="date">
+                            {{ $movie->date }}
+                        </x-slot>
+                        <x-slot name="vote">
+                            {{ $movie->vote }}
+                        </x-slot>
+                    </x-card>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
